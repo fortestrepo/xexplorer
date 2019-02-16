1500d05420a8dc866eca8091b3fc11b809a35d2c
ubot <ubot@xexplorer.com>
2019-02-16 11:10:40 +1100

    init project structure and repo

---
+++ src/test/java/com/utilitysoftwareservices/AppTestIT.java
@@ -0,0 +1,18 @@
+package com.utilitysoftwareservices;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTestIT {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
